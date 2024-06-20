c2c301b4b9f29f801470b59cd666d7d9f2ef732e
muskilon <max-post@yandex.ru>
2024-06-20 13:59:33 +0300

    Правки по боттомшиту

--- app/src/main/java/com/example/effectivemobiletestwork/avia/ui/AviaFragment.kt
+++ app/src/main/java/com/example/effectivemobiletestwork/avia/ui/AviaFragment.kt
@@ -1,22 +1,16 @@
 package com.example.effectivemobiletestwork.avia.ui
 
-import android.content.Context
-import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.example.effectivemobiletestwork.R
-import com.example.effectivemobiletestwork.app.App
 import com.example.effectivemobiletestwork.databinding.FragmentAviaBinding
 import com.example.effectivemobiletestwork.root.RootActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -39,7 +33,9 @@ class AviaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+        bottomSheetBehavior.skipCollapsed = true
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         bottomSheetBehavior.addBottomSheetCallback(object :
             BottomSheetBehavior.BottomSheetCallback() {
@@ -51,8 +47,7 @@ class AviaFragment : Fragment() {
                     else -> (activity as RootActivity).hideNavigation(false)
                 }
             }
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+            override fun onSlide(bottomSheet: View, slideOffset: Float) = Unit
         })
 
         binding.to.onFocusChangeListener = View.OnFocusChangeListener { _, isFocus ->
@@ -65,14 +60,34 @@ class AviaFragment : Fragment() {
             binding.offersRecycler.context,
             RecyclerView.HORIZONTAL
         )
-        binding.offersRecycler.adapter = aviaAdapter
-        binding.offersRecycler.addItemDecoration(
-            divider.apply {
-                setDividerThicknessResource(requireContext(),R.dimen.divider)
-                setDividerColorResource(requireContext(),R.color.black)
-                isLastItemDecorated = false
+        with(binding) {
+            offersRecycler.adapter = aviaAdapter
+            offersRecycler.addItemDecoration(
+                divider.apply {
+                    setDividerThicknessResource(requireContext(), R.dimen.divider)
+                    setDividerColorResource(requireContext(), R.color.black)
+                    isLastItemDecorated = false
+                }
+            )
+            from.addTextChangedListener(getTextWatcher())
+            include.from.addTextChangedListener(getTextWatcher())
+
+            include.everywhere.setOnClickListener {
+                include.to.setText(R.string.everywhere)
             }
-        )
+            include.istanbulClick.setOnClickListener {
+                include.to.setText(R.string.istanbul)
+            }
+            include.sochiClick.setOnClickListener {
+                include.to.setText(R.string.sochi)
+            }
+            include.phuketClick.setOnClickListener {
+                include.to.setText(R.string.phuket)
+            }
+            include.clear.setOnClickListener{
+                include.to.setText(String())
+            }
+        }
 
         viewModel.getRecommendations()
         viewModel.observeOffers().observe(viewLifecycleOwner){ offers ->
@@ -81,5 +96,15 @@ class AviaFragment : Fragment() {
 
         viewModel.getTickets()
         viewModel.getTicketsOffers()
+    }
+
+    private fun getTextWatcher() = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) = Unit
     }
 }
