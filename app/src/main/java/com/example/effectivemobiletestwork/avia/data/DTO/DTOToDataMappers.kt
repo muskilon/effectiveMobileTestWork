4adaf3d180ed0db054c6c504edcf61940c206caa
muskilon <max-post@yandex.ru>
2024-06-20 19:22:10 +0300

    форматирование вывода времени и цены

--- app/src/main/java/com/example/effectivemobiletestwork/avia/data/DTO/DTOToDataMappers.kt
+++ app/src/main/java/com/example/effectivemobiletestwork/avia/data/DTO/DTOToDataMappers.kt
@@ -2,6 +2,7 @@ package com.example.effectivemobiletestwork.avia.data.DTO
 
 import com.example.effectivemobiletestwork.avia.domain.model.Offer
 import com.example.effectivemobiletestwork.avia.domain.model.TicketsOffer
+import java.text.NumberFormat
 
 class DTOToDataMappers {
     fun recommendationsDTOToMainRecommendations(offers: List<OfferDTO>): List<Offer>{
@@ -26,10 +27,28 @@ class DTOToDataMappers {
             val newOffers = TicketsOffer(
                 id = it.id,
                 title = it.title,
-                price = it.price.value,
-                timeRange = it.timeRange.toString()
+                price = getPriceString(it.price.value.toInt()),
+                timeRange = getTimeRangeString(it.timeRange)
             )
             newOffers
         }
+    }
+
+    private fun getTimeRangeString(array: ArrayList<String>): String {
+        val timeRange = StringBuilder()
+        array.forEachIndexed { index, time ->
+            timeRange.append(time)
+            if(index != array.size-1) timeRange.append("  ")
+        }
+        return timeRange.toString()
+    }
+
+    private fun getPriceString(price: Int): String {
+        val result = StringBuilder()
+        with(result) {
+            append(NumberFormat.getInstance().format(price))
+            append(" ₽")
+        }
+        return result.toString()
     }
 }
