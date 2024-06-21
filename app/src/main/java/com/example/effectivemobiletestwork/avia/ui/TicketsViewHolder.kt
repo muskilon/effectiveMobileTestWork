b836f7eaca1903f2f56da9752ce6bbb8f5193497
muskilon <max-post@yandex.ru>
2024-06-21 12:52:49 +0300

    верстка бейджей для билетов

--- app/src/main/java/com/example/effectivemobiletestwork/avia/ui/TicketsViewHolder.kt
+++ app/src/main/java/com/example/effectivemobiletestwork/avia/ui/TicketsViewHolder.kt
@@ -1,6 +1,10 @@
 package com.example.effectivemobiletestwork.avia.ui
 
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.RecyclerView
+import com.example.effectivemobiletestwork.R
 import com.example.effectivemobiletestwork.avia.domain.model.Ticket
 import com.example.effectivemobiletestwork.databinding.ItemTicketBinding
 
@@ -8,6 +12,7 @@ class TicketsViewHolder(
     private val binding: ItemTicketBinding
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(model: Ticket) {
+        val marginTop = itemView.resources.getDimension(R.dimen.margin_top_ticket)
         with(binding) {
             price.text = model.price
             departureTime.text = model.departureDate
@@ -15,6 +20,17 @@ class TicketsViewHolder(
             arrivalTime.text = model.arrivalDate
             arrivalAirport.text = model.arrivalAirport
             options.text = model.options
+        }
+        if (model.badge.isNullOrBlank()) {
+            binding.bage.isVisible = false
+            binding.cardTicket.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                setMargins(0, marginTop.toInt(), 0, 0)
+            }
+        }
+        else {
+            binding.bage.isVisible = true
+            binding.bage.text = model.badge
+
         }
     }
 }
