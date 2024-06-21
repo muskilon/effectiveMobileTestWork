9960c607b1dba29a463a773b8ef9aac467954f05
muskilon <max-post@yandex.ru>
2024-06-21 11:20:55 +0300

    отформатировал данные для билетов

--- app/src/main/java/com/example/effectivemobiletestwork/avia/data/MockAPI.kt
+++ app/src/main/java/com/example/effectivemobiletestwork/avia/data/MockAPI.kt
@@ -7,6 +7,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface MockAPI {
+    // из задания
     @GET("ad9a46ba-276c-4a81-88a6-c068e51cce3a")
     suspend fun getRecommendations(): Response<MainRecommendationDTO>
 
@@ -15,6 +16,16 @@ interface MockAPI {
 
     @GET("c0464573-5a13-45c9-89f8-717436748b69")
     suspend fun getTickets(): Response<TicketsDTO>
+
+    // мои
+//    @GET("112219af-8eae-4480-b442-707219de1f1b")
+//    suspend fun getRecommendations(): Response<MainRecommendationDTO>
+//
+//    @GET("e82baab1-b1f3-4f70-a05a-ff841b323300")
+//    suspend fun getTicketsOffers(): Response<TicketsOffersDTO>
+//
+//    @GET("c914a35e-8d57-4363-a1cf-1d6127cc5be8")
+//    suspend fun getTickets(): Response<TicketsDTO>
 }
 
 // 38b5205d-1a3d-4c2f-9d77-2f9d1ef01a4a tickets_offers в задании
--- app/src/main/java/com/example/effectivemobiletestwork/avia/data/dto/DTOToDataMappers.kt
+++ app/src/main/java/com/example/effectivemobiletestwork/avia/data/dto/DTOToDataMappers.kt
@@ -4,6 +4,13 @@ import com.example.effectivemobiletestwork.avia.domain.model.Offer
 import com.example.effectivemobiletestwork.avia.domain.model.Ticket
 import com.example.effectivemobiletestwork.avia.domain.model.TicketsOffer
 import java.text.NumberFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.DurationUnit
 
 class DTOToDataMappers {
     fun recommendationsDTOToMainRecommendations(offers: List<OfferDTO>): List<Offer>{
@@ -25,11 +32,11 @@ class DTOToDataMappers {
                 id = it.id,
                 price = getPriceString(it.price.value.toInt()),
                 arrivalAirport = it.arrival.airport,
-                arrivalDate = it.arrival.date,
-                departureDate = it.departure.date,
+                arrivalDate = getDate(it.arrival.date),
+                departureDate = getDate(it.departure.date),
                 departureAirport = it.departure.airport,
-                timeInFlight = "СДЕЛАТЬ",
-                options = "СДЕЛАТЬ",
+                timeInFlight = getDuration(it.departure.date, it.arrival.date),
+                options = getOptions(it.hasTransfer, getDuration(it.departure.date, it.arrival.date)),
                 badge = it.badge,
                 transfer = it.hasTransfer
             )
@@ -47,6 +54,32 @@ class DTOToDataMappers {
             )
             newOffers
         }
+    }
+    private fun getOptions(transfer: Boolean, duration: String): String {
+        val options = StringBuilder()
+        options.append("${duration}ч в пути")
+        if (!transfer) options.append(" / Без пересадок")
+        return options.toString()
+    }
+
+    private fun getDuration(departure: String, arrival: String): String {
+        val diff = convertToDate(arrival).time - convertToDate(departure).time
+        val hours = diff.milliseconds.toDouble(DurationUnit.HOURS)
+        return String.format(Locale.US,"%.1f", hours)
+    }
+
+    private fun getDate(date: String): String {
+        return SimpleDateFormat("HH:mm", Locale.getDefault()).format(convertToDate(date))
+    }
+
+    private fun convertToDate(date: String): Date {
+        val dateFormat = try {
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault()).parse(date)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        return if (dateFormat is Date) dateFormat
+        else Calendar.getInstance().time
     }
 
     private fun getTimeRangeString(array: ArrayList<String>): String {
