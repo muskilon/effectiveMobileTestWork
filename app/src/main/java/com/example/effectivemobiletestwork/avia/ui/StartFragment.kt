6258632a663689fb11467edc6c558b96dfd983ea
muskilon <max-post@yandex.ru>
2024-06-24 11:18:43 +0300

    исправил баг навигации

---
+++ app/src/main/java/com/example/effectivemobiletestwork/avia/ui/StartFragment.kt
@@ -0,0 +1,27 @@
+package com.example.effectivemobiletestwork.avia.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.effectivemobiletestwork.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
